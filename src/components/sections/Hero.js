21e7c288c3299cdb47624bb0de607ef0d97dabee
HossamAhmed <hosamzain@gmail.com>
2022-06-28 14:58:18 +0200

    team image

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -87,7 +87,7 @@ const Hero = ({
             >
               <Image
                 className="has-shadow"
-                src={require('./../../assets/images/video-placeholder.jpg')}
+                src={require('./../../assets/images/team.jpg')}
                 alt="Hero"
                 width={896}
                 height={504} />
